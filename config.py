adfa00210985360fb1c9d5d6029d515132a0aa48
sohel-7 <manaurisohel.871@gmail.com>
2024-11-24 18:43:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY RAJOWNER90
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7576524422:AAHwgMSordjO_7GlN1-S02Q2-DhL7DvkJzU")
+BOT_TOKEN=("7240194618:AAGs0ezAbyPiqfiLBhWbb-5mum7qsRmnMNg")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("@GODNEXTYT")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("1549748318")
+ADMIN_IDS=("6654576379")
